37514c04057254fe2fec2ada323ed5211012c3a9
isaque <isaquediniz14@gmail.com>
2023-01-09 20:12:22 -0300

    feat(cqrs): craido Query e Handler de busca de filmes
    Criado GetMoviesQueryRequest e GetMoviesQueryRequestHandler

--- FandomStarWars.Application/CQRS/Movie/Handlers/GetMoviesQueryRequestHandler.cs
+++ FandomStarWars.Application/CQRS/Movie/Handlers/GetMoviesQueryRequestHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using FandomStarWars.Application.CQRS.BaseResponses;
 using FandomStarWars.Application.CQRS.Films.Requests.Querys;
+using FandomStarWars.Application.DTO_s;
+using FandomStarWars.Domain.Interfaces;
 using MediatR;
 
 namespace FandomStarWars.Application.CQRS.Films.Handlers
@@ -8,22 +10,36 @@ namespace FandomStarWars.Application.CQRS.Films.Handlers
     public class GetMoviesQueryRequestHandler : IRequestHandler<GetMoviesQueryRequest, GenericResponse>
     {
         private readonly IMapper _mapper;
+        private readonly IMovieRepository _repository;
 
-        public GetMoviesQueryRequestHandler(IMapper mapper)
+        public GetMoviesQueryRequestHandler(IMapper mapper, IMovieRepository repository)
         {
             mapper = mapper;
+            _repository = repository;
         }
 
-        public Task<GenericResponse> Handle(GetMoviesQueryRequest request, CancellationToken cancellationToken)
+        public async Task<GenericResponse> Handle(GetMoviesQueryRequest request, CancellationToken cancellationToken)
         {
             try
             {
-                throw new Exception();
-            }
-            catch (Exception)
-            {
+                var moviesEntity = await _repository.GetAllAsync();
+                var moviesDTO = _mapper.Map<MovieDTO>(moviesEntity);
 
-                throw;
+                return new GenericResponse
+                {
+                    IsSuccessful = true,
+                    Message = moviesEntity is not null ? "Successfully obtained Movies" : "Not found Movies",
+                    Object = moviesDTO
+                };
+                
+            }
+            catch (Exception e)
+            {
+                return new GenericResponse
+                {
+                    IsSuccessful = false,
+                    Message = "Error " + e.Message
+                };
             }
         }
     }

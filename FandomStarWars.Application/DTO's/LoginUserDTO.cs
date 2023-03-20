197f3cc33f2281c7737667f58e50abd9eaaefe73
isaque14 <isaquediniz14@gmail.com>
2023-03-20 19:54:08 -0300

    feat(registeruserdto e loginuserdto): criado os DTO's para registrar usuário e relizar login

---
+++ FandomStarWars.Application/DTO's/LoginUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FandomStarWars.Application.DTO_s
+{
+    public class LoginUserDTO
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato inválido do campo {0}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "Tamanho máximo do campo {0} é 100")]
+        public string Password { get; set; }
+    }
+}
---
+++ FandomStarWars.Application/DTO's/RegisterUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FandomStarWars.Application.DTO_s
+{
+    public class RegisterUserDTO
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato inválido do campo {0}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "Tamanho máximo do campo {0} é 100")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Senhas não conferem")]
+        public string ConfirmPassword { get; set; }
+    }
+}

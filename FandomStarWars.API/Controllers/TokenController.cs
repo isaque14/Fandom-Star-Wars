6efdf33683dc9c8b3f1abe610c10720cacdf46ea
isaque14 <isaquediniz14@gmail.com>
2023-03-21 12:07:13 -0300

    feat(token jwt): gerando token jwt

--- FandomStarWars.API/Controllers/TokenController.cs
+++ FandomStarWars.API/Controllers/TokenController.cs
@@ -3,6 +3,10 @@ using FandomStarWars.Application.DTO_s;
 using FandomStarWars.Domain.Account;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace FandomStarWars.API.Controllers
 {
@@ -11,10 +15,12 @@ namespace FandomStarWars.API.Controllers
     public class TokenController : ControllerBase
     {
         private readonly IAuthenticate _authentication;
+        private readonly IConfiguration _configuration;
 
-        public TokenController(IAuthenticate authentication)
+        public TokenController(IAuthenticate authentication, IConfiguration configuration)
         {
             _authentication = authentication;
+            _configuration = configuration;
         }
 
         [HttpPost("LoginUser")]
@@ -23,15 +29,53 @@ namespace FandomStarWars.API.Controllers
             var result = await _authentication.Authenticate(userInfo.Email, userInfo.Password);
 
             if (result)
-            {
-                //return GenerateToken(userInfo);
-                return Ok($"User {userInfo.Email} login successfully");
-            }
+                return GenerateToken(userInfo);
+
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
                 return BadRequest(ModelState);
             }
+        }
+
+        private UserToken GenerateToken(LoginUserModel userInfo)
+        {
+            //declarações do usuário
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //gerar chave privada para assinar o token
+            var privateKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["JWT:SecretKey"]));
+
+            //gerar a assinatura digital
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //definir o tempo de expiração
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //gerar o token
+            JwtSecurityToken token = new JwtSecurityToken(
+                //emissor
+                issuer: _configuration["JWT:Issuer"],
+                //audiencia
+                audience: _configuration["JWT:Audience"],
+                //claims
+                claims: claims,
+                //data de expiracao
+                expires: expiration,
+                //assinatura digital
+                signingCredentials: credentials
+                );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
         }
     }
 }
